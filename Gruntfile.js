3ef1804d781db1686e9be23da8f9bf3a4779d56e
oz.ghost <xad.love@gmail.com>
2018-09-06 20:56:10 +0700

    create folder structure and some index file

---
+++ Gruntfile.js
@@ -0,0 +1,77 @@
+
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    sass: {
+      options: {
+        sourceMap: false
+      },
+      dist: {
+        files: {
+          './dest/index.css': './src/style/index.sass'
+        }
+      }
+    },
+    babel: {
+      options: {
+        presets: ['env', 'react', 'flow'],
+        plugins: [
+          'transform-react-jsx',
+          'transform-es2015-classes',
+          'transform-es2015-spread',
+          'transform-es2015-arrow-functions',
+        ]
+      },
+      dist: {
+        files: {
+          './dest/index.js': './src/engine/index.js',
+          './dest/**/*.js': './src/engine/**/*.js'
+        }
+      }
+    },
+    copy: {
+      frame: {
+        files: [
+          {
+            expand: true,
+            cwd: 'src',
+            src: ['index.html', 'configuration.js'],
+            dest: './dest/'
+          }
+        ]
+      }
+    },
+    browserify: {
+      dist: {
+        files: {
+          './dest/index.js': './src/engine/index.js',
+        },
+        options: {
+          transform: ['babelify']
+        }
+      }
+    },
+    watch: {
+      script: {
+        files: ["./src/engine/*.js", "./src/engine/**/*.js"],
+        tasks: ['browserify']
+      },
+      style: {
+        files: './src/style/*.sass',
+        tasks: ['sass']
+      },
+      frame: {
+        files: './src/index.html',
+        tasks: ['copy']
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-browserify');
+
+  grunt.registerTask('default', ['sass', 'copy', 'browserify']);
+};
+
